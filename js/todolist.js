75566d6b6e2deeda5b54dce0370fafc394b87679
Yanius27 <97672725+Yanius27@users.noreply.github.com>
2022-10-27 09:25:13 +0700

    Add files via upload

--- js/todolist.js
+++ js/todolist.js
@@ -1,1 +1,13 @@
-console.log('first');
+let values = [];
+
+document.getElementById('add').addEventListener('click', () => {
+  let val = document.getElementById('input').value;
+  values.push(val);
+
+  const taskLi = values.forEach(v => {
+    const li = document.createElement('li');
+    li.textContent = v;
+    document.getElementById('taskList').appendChild(li);
+  });
+
+});
