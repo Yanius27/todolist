76a86890e86d87881480aa38069af9e361fcf17d
Yanius27 <97672725+Yanius27@users.noreply.github.com>
2022-11-08 22:15:38 +0700

    Add files via upload
    Logic-Done-Commit

--- js/todolist.js
+++ js/todolist.js
@@ -1,18 +1,45 @@
-const tasks = [];
+let tasks = [];
 
-const liActual = (val) => {
-  return `<li class="task-block">${val}</li>`;
+const startUp = () => {
+  document.getElementById('add').addEventListener('click', saveTaskAndDateToObject);
+  document.getElementById('taskList').addEventListener('click', (event) => {
+    let id = event.target.getAttribute('data-task-id');
+    tasks = tasks.filter((task) => {
+      return task.id != id;
+    });
+    renderTaskList();
+  });
+}
+
+const taskActual = (val) => {
+  return `<div class="task-block">
+    <div class="task">${val.task}</div>
+    <div class="task-content">
+      <div class="date">${val.date}</div>
+      <button type="reset" class="button-reset" data-task-id=${val.id}>
+        <span class="hidden">Кнопка для удаления задачи</span>
+      </button>
+    </div>
+  </div>`;
 }
 
 const renderTaskList = () => {
-  document.getElementById('taskList').innerHTML = tasks.map(liActual).join('');
+  document.getElementById('taskList').innerHTML = tasks.map(taskActual).join('');
 }
 
-
-const saveTask = () => {
-  const field = document.getElementById('input').value;
-  tasks.push(field);
+const addToMassiv = (f) => {
+  tasks.push(f);
   renderTaskList();
 }
 
-document.getElementById('add').addEventListener('click', saveTask);
+const createId = () => Math.random();
+
+const saveTaskAndDateToObject = () => {
+  const field = {};
+  field.task = document.getElementById('input').value;
+  field.date = new Date().toLocaleDateString();
+  field.id = createId();
+  addToMassiv(field);
+}
+
+startUp();
